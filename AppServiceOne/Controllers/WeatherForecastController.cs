7f97047e2af7184f8eeb8fc38b73a105321b2669
Ros <Ros@DESKTOP-FHTAGLA>
2022-04-20 19:49:48 +0300

    main app

--- AppServiceOne/Controllers/WeatherForecastController.cs
+++ AppServiceOne/Controllers/WeatherForecastController.cs
@@ -59,58 +59,58 @@ namespace AppServiceOne.Controllers
             return true;
         }
 
-        [HttpGet(Name = "GetExcel")]
-        public IActionResult GetExcel()
-        {
-            string fileName = "ExcelList-" + Guid.NewGuid().ToString() + ".xlsx";
+        //[HttpGet(Name = "GetExcel")]
+        //public IActionResult GetExcel()
+        //{
+        //    string fileName = "ExcelList-" + Guid.NewGuid().ToString() + ".xlsx";
 
-            List<ContactInfo>? list = new List<ContactInfo>();
-            using (var connection = factory.CreateConnection())
-            using (var channel = connection.CreateModel())
-            {
-                var consumer = new AsyncEventingBasicConsumer(channel);
-                consumer.Received += async (ch, ea) =>
-                {
-                    var body = ea.Body.ToArray();
-                    var text = System.Text.Encoding.UTF8.GetString(body);
-                    await Task.CompletedTask;
-                    list.Add(new ContactInfo { name = text });
-                    channel.BasicAck(ea.DeliveryTag, false);
-                };
-                channel.BasicConsume("teatqueue", false, consumer);
-            }
-            using (MemoryStream stream = new MemoryStream())
-            {
-                IWorkbook workbook = new XSSFWorkbook();
-                ISheet excelSheet = workbook.CreateSheet(fileName);
-                List<string> columns = new List<string>();
-                IRow row = excelSheet.CreateRow(0);
-                int columnIndex = 0;
+        //    List<ContactInfo>? list = new List<ContactInfo>();
+        //    using (var connection = factory.CreateConnection())
+        //    using (var channel = connection.CreateModel())
+        //    {
+        //        var consumer = new AsyncEventingBasicConsumer(channel);
+        //        consumer.Received += async (ch, ea) =>
+        //        {
+        //            var body = ea.Body.ToArray();
+        //            var text = System.Text.Encoding.UTF8.GetString(body);
+        //            await Task.CompletedTask;
+        //            list.Add(new ContactInfo { name = text });
+        //            channel.BasicAck(ea.DeliveryTag, false);
+        //        };
+        //        channel.BasicConsume("teatqueue", false, consumer);
+        //    }
+        //    using (MemoryStream stream = new MemoryStream())
+        //    {
+        //        IWorkbook workbook = new XSSFWorkbook();
+        //        ISheet excelSheet = workbook.CreateSheet(fileName);
+        //        List<string> columns = new List<string>();
+        //        IRow row = excelSheet.CreateRow(0);
+        //        int columnIndex = 0;
 
-                //foreach (System.Data.DataColumn column in dataTable.Columns)
-                {
-                    columns.Add("Name");//(column.ColumnName);
-                    row.CreateCell(columnIndex).SetCellValue("Name");//(column.ColumnName);
-                    columnIndex++;
-                }
+        //        //foreach (System.Data.DataColumn column in dataTable.Columns)
+        //        {
+        //            columns.Add("Name");//(column.ColumnName);
+        //            row.CreateCell(columnIndex).SetCellValue("Name");//(column.ColumnName);
+        //            columnIndex++;
+        //        }
 
-                int rowIndex = 1;
-                foreach (var dsrow in list)
-                {
-                    row = excelSheet.CreateRow(rowIndex);
-                    int cellIndex = 0;
-                    foreach (String col in columns)
-                    {
-                        row.CreateCell(cellIndex).SetCellValue(dsrow.name.ToString());
-                        cellIndex++;
-                    }
+        //        int rowIndex = 1;
+        //        foreach (var dsrow in list)
+        //        {
+        //            row = excelSheet.CreateRow(rowIndex);
+        //            int cellIndex = 0;
+        //            foreach (String col in columns)
+        //            {
+        //                row.CreateCell(cellIndex).SetCellValue(dsrow.name.ToString());
+        //                cellIndex++;
+        //            }
 
-                    rowIndex++;
-                }
+        //            rowIndex++;
+        //        }
 
-                workbook.Write(stream);
-                return File(stream, "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", fileName);
-            }
-        }
+        //        workbook.Write(stream);
+        //        return File(stream, "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", fileName);
+        //    }
+        //}
     }
 }
---
+++ WebAppMain/Controllers/HomeController.cs
@@ -0,0 +1,93 @@
+﻿using AppCore.Models;
+using Microsoft.AspNetCore.Mvc;
+using NPOI.SS.UserModel;
+using NPOI.XSSF.UserModel;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Diagnostics;
+using WebAppMain.Models;
+
+namespace WebAppMain.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private ConnectionFactory factory;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+            factory = new ConnectionFactory()
+            {
+                HostName = "192.168.43.97",
+                UserName = "eren",
+                Password = "eren",
+            };
+        }
+
+        public IActionResult Index()
+        {
+            string fileName = "ExcelList-" + Guid.NewGuid().ToString() + ".xlsx";
+
+            List<ContactInfo>? list = new List<ContactInfo>();
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                var consumer = new AsyncEventingBasicConsumer(channel);
+                consumer.Received += async (ch, ea) =>
+                {
+                    var body = ea.Body.ToArray();
+                    var text = System.Text.Encoding.UTF8.GetString(body);
+                    await Task.CompletedTask;
+                    list.Add(new ContactInfo { name = text });
+                    channel.BasicAck(ea.DeliveryTag, false);
+                };
+                channel.BasicConsume("teatqueue", false, consumer);
+            }
+            using (MemoryStream stream = new MemoryStream())
+            {
+                IWorkbook workbook = new XSSFWorkbook();
+                ISheet excelSheet = workbook.CreateSheet(fileName);
+                List<string> columns = new List<string>();
+                IRow row = excelSheet.CreateRow(0);
+                int columnIndex = 0;
+
+                //foreach (System.Data.DataColumn column in dataTable.Columns)
+                {
+                    columns.Add("Name");//(column.ColumnName);
+                    row.CreateCell(columnIndex).SetCellValue("Name");//(column.ColumnName);
+                    columnIndex++;
+                }
+
+                int rowIndex = 1;
+                foreach (var dsrow in list)
+                {
+                    row = excelSheet.CreateRow(rowIndex);
+                    int cellIndex = 0;
+                    foreach (String col in columns)
+                    {
+                        row.CreateCell(cellIndex).SetCellValue(dsrow.name.ToString());
+                        cellIndex++;
+                    }
+
+                    rowIndex++;
+                }
+
+                workbook.Write(stream);
+                return File(stream, "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", fileName);
+            }
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
