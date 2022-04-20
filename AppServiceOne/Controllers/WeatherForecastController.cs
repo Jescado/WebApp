16f7630373bafbce12dee493861bc71fe388eddc
Ros <Ros@DESKTOP-FHTAGLA>
2022-04-20 16:17:18 +0300

    rabbitmq added

--- AppServiceOne/Controllers/WeatherForecastController.cs
+++ AppServiceOne/Controllers/WeatherForecastController.cs
@@ -1,4 +1,9 @@
+using AppCore.Models;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
 
 namespace AppServiceOne.Controllers
 {
@@ -6,11 +11,6 @@ namespace AppServiceOne.Controllers
     [Route("[controller]")]
     public class WeatherForecastController : ControllerBase
     {
-        private static readonly string[] Summaries = new[]
-        {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-    };
-
         private readonly ILogger<WeatherForecastController> _logger;
 
         public WeatherForecastController(ILogger<WeatherForecastController> logger)
@@ -18,16 +18,53 @@ namespace AppServiceOne.Controllers
             _logger = logger;
         }
 
-        [HttpGet(Name = "GetWeatherForecast")]
-        public IEnumerable<WeatherForecast> Get()
+        [HttpGet(Name = "GetContactInfo")]
+        public IEnumerable<ContactInfo> Get()
         {
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            var list = new List<ContactInfo>();
+            ContactInfo contactInfo = new ContactInfo
             {
-                Date = DateTime.Now.AddDays(index),
-                TemperatureC = Random.Shared.Next(-20, 55),
-                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-            })
-            .ToArray();
+                address = new List<Address>(),
+                company = "test",
+                name = "test",
+                surName = "test"
+            };
+
+            var factory = new ConnectionFactory()
+            {
+                HostName = "192.168.43.97",
+                UserName = "eren",
+                Password = "eren",
+            };
+
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                //channel.QueueDeclare(queue: "teatqueue",
+                //                     durable: false,
+                //                     exclusive: false,
+                //                     autoDelete: false,
+                //                     arguments: null);
+
+                var message = JsonConvert.SerializeObject(contactInfo);
+                var body = Encoding.UTF8.GetBytes(message);
+
+                channel.BasicPublish(exchange: "",
+                                                 routingKey: "teatqueue",
+                                                 basicProperties: null,
+                                                 body: body);
+
+
+                var consumer = new EventingBasicConsumer(channel);
+                consumer.Received += (model, eventArgs) =>
+                {
+                    var body = eventArgs.Body.ToArray();
+                    var message = Encoding.UTF8.GetString(body);
+                    list.Add(new ContactInfo { name=message});
+                };
+
+                return list;
+            }
         }
     }
 }
